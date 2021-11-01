7f253d0e6ed3ca3d9b3ad4055d3cfdf8a7decc3d
HamzaYousuf7 <hamzayousuf175@gmail.com>
2021-11-01 10:04:02 +0500

    transfering all the token to ethSwap funtionality

--- migrations/2_deploy_contract.js
+++ migrations/2_deploy_contract.js
@@ -1,8 +1,15 @@
 const EthSwap = artifacts.require("EthSwap");
 const Token = artifacts.require("Token");
 
-module.exports = (deployer) => {
-  //
-  deployer.deploy(EthSwap);
-  deployer.deploy(Token);
+module.exports = async (deployer) => {
+  //token
+  await deployer.deploy(Token);
+  const token = await Token.deployed();
+
+  //ethswap
+  await deployer.deploy(EthSwap);
+  const ethSwap = await EthSwap.deployed();
+
+  // transfering all the token to ethSwap
+  await token.transfer(ethSwap.address, "1000000000000000000000000");
 };
