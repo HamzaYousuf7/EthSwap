d99b2f979375c0634a6a0a7c324c05be1bf310f3
HamzaYousuf7 <hamzayousuf175@gmail.com>
2021-11-01 09:50:50 +0500

    added Alpha token smart contract

--- migrations/2_deploy_contract.js
+++ migrations/2_deploy_contract.js
@@ -1,5 +1,8 @@
 const EthSwap = artifacts.require("EthSwap");
+const Token = artifacts.require("Token");
 
 module.exports = (deployer) => {
+  //
   deployer.deploy(EthSwap);
+  deployer.deploy(Token);
 };
